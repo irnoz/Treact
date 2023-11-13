fb2f63c7bf9d9b3e81cc996686fb45ea0376b28f
irnoz <iraklinozadze2509@gmail.com>
2023-11-13 15:10:22 +0400

    feat: add start button

--- Treact/ViewController.swift
+++ Treact/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timerButton: UIButton!
+    
+    var timerStarted = false
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .black
+        
+        timerButton = UIButton(type: .system)
+        timerButton.translatesAutoresizingMaskIntoConstraints = false
+        timerButton.setTitle("START", for: .normal)
+        view.addSubview(timerButton)
+        timerButton.addTarget(self, action: #selector(startTapped), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            timerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            timerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func startTapped(_ sender: UIButton) {
+        print("button tapped")
+    }
 
 }
 
