fed668b67ba3356743be9446732b9a505f6e7ac6
irnoz <iraklinozadze2509@gmail.com>
2023-11-13 15:36:07 +0400

    cleanup

--- Treact/ViewController.swift
+++ Treact/ViewController.swift
@@ -29,25 +29,16 @@ class ViewController: UIViewController {
         view.addSubview(timerButton)
         timerButton.addTarget(self, action: #selector(startTapped), for: .touchUpInside)
         
-//        // Add padding to the button's frame
-//        let padding: CGFloat = 10.0
-//        timerButton.frame = CGRect(x: timerButton.frame.origin.x - padding, y: timerButton.frame.origin.y - padding, width: timerButton.frame.size.width + 2 * padding, height: timerButton.frame.size.height + 2 * padding)
-//
-//        // Make the button fully circular
-//        timerButton.layer.cornerRadius = timerButton.frame.size.height / 2.0
-//        timerButton.layer.borderWidth = 1.0
-//        timerButton.layer.borderColor = UIColor.blue.cgColor
-        
-        timerButton.layer.borderColor = UIColor.red.cgColor
+        timerButton.layer.borderColor = UIColor.blue.cgColor
         timerButton.layer.borderWidth = 1.5
-        timerButton.layer.cornerRadius = 25.0 // height / 2
+        timerButton.layer.cornerRadius = 50.0 // height / 2
         timerButton.layer.masksToBounds = true
         
         NSLayoutConstraint.activate([
             timerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             timerButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            timerButton.heightAnchor.constraint(equalToConstant: 50),
-            timerButton.widthAnchor.constraint(equalToConstant: 50)
+            timerButton.heightAnchor.constraint(equalToConstant: 100),
+            timerButton.widthAnchor.constraint(equalToConstant: 100)
         ])
     }
     
